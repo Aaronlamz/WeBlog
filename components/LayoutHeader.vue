d285195fdaf10a200fa35b2a63ba85437d81a90d
aaronlamz <aaronlamz2022@gmail.com>
2022-08-25 22:33:25 +0800

    fix header

--- components/LayoutHeader.vue
+++ components/LayoutHeader.vue
@@ -1,3 +1,20 @@
 <template lang="pug">
-div LayoutHeader
-</template>>
+header.weblog-header
+  .weblog-nav
+    .container
+      .title
+        .logo logo
+        .text Weblog
+</template>
+<style lang="less" >
+.weblog-nav{
+  padding: 0 32px;
+  height: 56px;
+  border-bottom: 1px solid #ccc;
+  .container{
+    display: flex;
+    height: 100%;
+    align-items: center;
+  }
+}
+</style>
