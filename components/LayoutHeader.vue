f5177aafe67e1ef62c0a80e6f611124d25c3281f
aaronlamz <aaronlamz2022@gmail.com>
2022-09-11 17:08:34 +0800

    add vite config

--- components/LayoutHeader.vue
+++ components/LayoutHeader.vue
@@ -3,11 +3,14 @@ header.w-header
   .top-nav
     .logo
       img(src='~/assets/images/logo.jpeg' alt="logo")
-    .text Weblog
+    .title Aaron Lam's Space
 </template>
 <style lang="less" scoped>
 .top-nav {
+  padding: 16px;
   display: flex;
+  background-color: @primary-color;
+  color: @font-white;
   align-items: center;
   .logo {
     border-radius: 50%;
@@ -16,6 +19,10 @@ header.w-header
       display: block;
       height: 36px;
     }
+  }
+  .title {
+    padding-left: 16px;
+    font-weight: 500;
   }
 }
 </style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,13 @@ import { defineNuxtConfig } from 'nuxt';
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   buildModules: ['@nuxt/typescript-build', '@nuxtjs/stylelint-module'],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        less: {
+          additionalData: '@import "@/assets/style/variables.less";',
+        },
+      },
+    },
+  },
 });
