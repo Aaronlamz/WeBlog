2ea07081da8ce554eab13144d9169c2b51666871
aaronlamz <aaronlamz2022@gmail.com>
2022-09-11 10:17:40 +0800

    update

--- components/LayoutHeader.vue
+++ components/LayoutHeader.vue
@@ -1,29 +1,18 @@
 <template lang="pug">
-header.weblog-header
-  .weblog-nav
-    .container
-      .top-nav
-        .logo
-          img(src='~/assets/images/logo.jpeg')
-        .text Weblog
+header.w-header
+  .top-nav
+    .logo
+      img(src='~/assets/images/logo.jpeg')
+    .text Weblog
 </template>
 <style lang="less" scoped>
-.weblog-nav {
-  padding: 0 32px;
-  border-bottom: 1px solid #ccc;
-  .container {
-    display: flex;
-    height: 100%;
-    align-items: center;
-  }
-  .top-nav {
-    display: flex;
-    align-items: center;
-    .logo {
-      img {
-        display: block;
-        height: 36px;
-      }
+.top-nav {
+  display: flex;
+  align-items: center;
+  .logo {
+    img {
+      display: block;
+      height: 36px;
     }
   }
 }
