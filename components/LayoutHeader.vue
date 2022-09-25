c7863ee27bd6dac2b59a779d7df2121c4925c0a3
aaronlamz <aaronlamz2022@gmail.com>
2022-09-25 17:25:02 +0800

    update layout

--- components/LayoutHeader.vue
+++ components/LayoutHeader.vue
@@ -1,9 +1,14 @@
 <template lang="pug">
 header.w-header
   .top-nav
-    .logo
-      img(src='~/assets/images/logo.jpeg' alt="logo")
-    .title Aaron Lam's Space
+    .left-item
+      .logo
+        img(src='~/assets/images/logo.jpeg' alt="logo")
+      .title Aaron's Space
+    .right-item
+      .right-item__menu
+        .menu__item Tags
+        .menu__item About
 </template>
 <style lang="less" scoped>
 .top-nav {
@@ -12,17 +17,28 @@ header.w-header
   background-color: @primary-color;
   color: @font-white;
   align-items: center;
-  .logo {
-    border-radius: 50%;
-    overflow: hidden;
-    img {
-      display: block;
-      height: 36px;
+  justify-content: space-between;
+  .left-item {
+    display: flex;
+    align-items: center;
+    .logo {
+      border-radius: 50%;
+      overflow: hidden;
+      img {
+        display: block;
+        height: 36px;
+      }
+    }
+    .title {
+      padding-left: 16px;
+      font-weight: 500;
     }
   }
-  .title {
-    padding-left: 16px;
-    font-weight: 500;
+  .right-item__menu {
+    display: flex;
+    .menu__item {
+      padding: 0 10px;
+    }
   }
 }
 </style>
